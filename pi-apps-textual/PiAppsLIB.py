d8135c3d4c052981768bd759963cdd27ba453175
pi-dev500 <77228126+pi-dev500@users.noreply.github.com>
2024-06-27 14:32:51 +0200

    init commit

---
+++ pi-apps-textual/PiAppsLIB.py
@@ -0,0 +1,77 @@
+#!/bin/python
+import os
+import sys
+import subprocess
+import json
+
+def error(message,code=1):
+    print(message)
+    quit(code)
+
+def parse_file_in_chunks(file_path, chunk_size=5):
+    chunks=[]
+    with open(file_path, 'r') as file:
+        chunk = []
+        for line in file:
+            chunk.append(line.strip())
+            if len(chunk) == chunk_size:
+                chunks.append(chunk)
+                chunk = []
+        # Process any remaining lines that didn't form a full chunk
+        if chunk:
+            chunks.append(chunk)
+    return chunks
+
+class PiAppsInstance:
+    def __init__(self, pi_apps_path):
+        self.path=pi_apps_path
+        self.apps=os.listdir(os.path.join(pi_apps_path,"apps"))
+        dir_lookup_shell=subprocess.check_output("grep -w '    dir_lookup=' "+os.path.join(self.path,"preload"),shell=True).decode().split("=")
+        dir_lookup_shell.pop(0)
+        self.dir_lookup=eval("=".join(dir_lookup_shell).replace('( [','{').replace(')','}').replace('[',',').replace(']=',':'))
+        #print(self.dir_lookup["All Apps"]) # make sure that dir_lookup was parsed correctly
+    def get_structure(self,place=""):
+        struc=list()
+        apps=[]
+        for element in parse_file_in_chunks(os.path.join(self.path,"data","preload","LIST-"+place.replace("/",""))):
+            parsed_element={}
+            if not element[1]=="Back":
+                parsed_element["icons"]={'icon-24': element[0]}
+                parsed_element["name"]=element[1]
+                parsed_element["value"]=element[2]
+                parsed_element["tooltip"]=element[3]
+                parsed_element["status_color"]=element[4]
+                if parsed_element["name"] in self.apps: # defines if it's an app or not
+                    parsed_element["type"]="Application"
+                    if place=="All Apps/":
+                        apps.append(parsed_element["name"])
+                else:
+                    parsed_element["type"]="Category"
+                    parsed_element["content"]=self.get_structure(parsed_element["value"])
+                struc.append(parsed_element)
+        self.structure=struc
+        if place=="All Apps/":
+            self.apps=apps
+        return struc
+    def get_app_details(self,data):
+        data["type"]="Application"
+        with open(os.path.join(self.path,"apps",data["name"],'description'),'r') as d:
+            data["description"]=[ l.replace("\n","") for l in d.readlines()]
+        try:
+            with open(os.path.join(self.path,"apps",data["name"],'credits'),'r') as c:
+                data["credits"]=[ l.replace("\n","") for l in c.readlines()]
+        except Exception:
+            data["credits"]=[]
+        try:
+            with open(os.path.join(self.path,"apps",data["name"],'website'),'r') as www:
+                data["website"]=www.readline().replace("\n","")
+        except Exception:
+            data["website"]=""
+        if os.path.exists(self.path + "/data/status/" + data["name"]):
+            with open(self.path + "/data/status/" + data["name"], 'r') as f_status:
+                data["status"]=f_status.readline()[:-1]
+        else:
+            data["status"]='uninstalled'
+        return data
+            
+instance=PiAppsInstance(os.path.expanduser("~/pi-apps"))
---
+++ pi-apps-textual/textualinterface.py
@@ -0,0 +1,200 @@
+from textual import on
+from textual.app import App, ComposeResult
+from textual.widgets import Label, Button, Header, OptionList, DataTable, Input, Static, Markdown, Collapsible, Footer, TabbedContent, Placeholder
+from textual.widgets.option_list import Option
+from textual.containers import Horizontal, Vertical, VerticalScroll
+from textual_terminal import Terminal
+import os
+import sys
+import webbrowser
+from PiAppsLIB import PiAppsInstance
+self_directory=os.path.dirname(os.path.realpath(sys.argv[0]))
+
+def X_is_running():
+    from subprocess import Popen, PIPE
+    p = Popen(["xset", "-q"], stdout=PIPE, stderr=PIPE)
+    p.communicate()
+    return p.returncode == 0
+
+class AppDisplay(Static):
+    app=None
+    piappsMD="""# Pi-Apps
+Let's be honest: **Linux is harder to master than Windows.** Sometimes it's not user-friendly, and following an outdated tutorial may break your Raspberry Pi's operating system.  
+There is no centralized software repository, except for the `apt` repositories which lack many desktop applications.  
+Surely there is a better way! **There is.**  
+Introducing Pi-Apps, a well-maintained collection of app installation-scripts that you can run with **one click**.  
+
+Pi-Apps now serves **over 1,000,000 people** and hosts [over 200 apps](https://pi-apps.io/wiki/getting-started/apps-list/)."""
+    piappscredits="""The store himself:
+Botspot
+Theofficialgman
+and many others...
+For this textual TUI:
+pi-dev500
+Textualize Team"""
+    def __init__(self,LIBinstance):
+        self.pi_apps_instance=LIBinstance
+        super().__init__()
+    def compose(self):
+        with VerticalScroll():
+            yield Markdown(self.piappsMD,id="APPMD")
+            with Collapsible(title="Credits",id='credits_collapsible'):
+                yield Label(self.piappscredits,id="credits")
+        with Horizontal(id="actions"):
+            yield Button.success("Install", id="install_button")
+            yield Button.error("Uninstall", id="uninstall_button")
+    def on_mount(self):
+        self.add_class("no_display_app")
+    def load_app(self,data):
+        self.app=data["name"]
+        data=self.pi_apps_instance.get_app_details(data)
+        md=self.query_one("#APPMD")
+        title="# "+data["name"]
+        if data["website"]=="":
+            website=""
+        else:
+            website=f"Website: "+data['website']
+        if data["credits"]==[]:
+            self.add_class("nocredits")
+        else:
+            self.remove_class("nocredits")
+            self.query_one("#credits").update("\n".join(data["credits"]))
+        self.remove_class("no_display_app")
+        if data["status"]=="installed":
+            self.remove_class("uninstalled_app")
+            self.add_class("installed_app")
+        elif data["status"]=="uninstalled":
+            self.remove_class("installed_app")
+            self.add_class("uninstalled_app")
+        md.update(title+"\n"+website+"\n"+"\n".join(data["description"]))
+    def on_markdown_link_clicked(self, event: Markdown.LinkClicked) -> None:
+        if X_is_running:
+            webbrowser.open(event.href)
+    def on_button_pressed(self, event: Button.Pressed):
+        if app and False:
+            if event.id=="install_button":
+                with open(os.path.join(self.pi_apps_instance.path,"data","manage-daemon","queue"),"a") as q:
+                    q.write("install;"+self.app)
+            elif event.id=="uninstall_button":
+                with open(os.path.join(self.pi_apps_instance.path,"data","manage-daemon","queue"),"a") as q:
+                    q.write("uninstall;"+self.app)
+    def revert(self):
+        md=self.query_one("#APPMD")
+        cre=self.query_one("#credits")
+        self.add_class("no_display_app")
+        self.remove_class("uninstalled_app")
+        self.remove_class("installed_app")
+        self.remove_class("nocredits")
+        md.update(self.piappsMD)
+        cre.update(self.piappscredits)
+        self.app=None
+
+class piapps(App):
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode"),("q","quit","Exit")]
+    CSS_PATH="basecss.css"
+    
+    directory=""
+    search=""
+    lib=PiAppsInstance(os.path.expanduser("~/pi-apps"))
+    structure=lib.get_structure()
+    def compose(self) -> ComposeResult:
+        yield Header()
+        with TabbedContent("Pi-Apps","Terminal"):
+            with Horizontal(id="pagelayout"):
+                with Vertical(id="browser"):
+                    yield Input(id="search")
+                    yield OptionList(*[Option(e,id=e) for e in self.get_page_infos()],id="app_browser")
+                self.appframe=AppDisplay(self.lib)
+                yield self.appframe
+            with Vertical(id="TermLayout"):
+                yield Label("The manage log will be shown there:", id="termlabel")
+                yield Placeholder("Waiting for terminal..", id="terminal_ph")
+        yield Footer()
+    def on_mount(self) -> None:
+        self.title = "Pi-Apps"
+        self.sub_title="Raspberry pi app store for open source projects"
+        
+    """@on(TabbedContent.TabActivated)
+    def mount_terminal(self):
+        try:
+            tph=self.query_one("#terminal_ph")
+            tph.remove()
+            term=Terminal(os.path.join(self_directory,"terminal-manage")+" "+self.lib.path,id="terminalog")
+            self.app.mount(term,after="#termlabel")
+            term.start()
+        except:
+            pass
+    #def on_ready(self):
+    #    self.mount_terminal()"""
+    def browseto(self,structure,place) -> list:
+        if place==[""] or len(place)==0:
+            return structure
+        else:
+            thistime=place.pop(0)
+            for e in structure:
+                if e["name"]==thistime:
+                    return self.browseto(e["content"],place)
+    def get_page_infos(self,place="") -> list:
+        self.directory_data={}
+        for e in self.browseto(self.structure,place.split('/')):
+            self.directory_data[e['name']]=e
+        if place=="":
+            return list(self.directory_data.keys())
+        else:
+            return ['← Back'] + list(self.directory_data.keys())
+    def load_app(self, selected)->None:
+        apps=self.browseto(self.structure,["All Apps"])
+        dapps={}
+        for app in apps:
+            dapps[app["name"]]=app
+        self.appframe.load_app(dapps[selected])
+    def on_option_list_option_selected(self, event: OptionList.OptionSelected) -> None:
+        selected_option = event.option_id
+        #with open("l.txt","w") as f: # log file
+        #    f.write(str(event.row_key.value))
+        ol = self.query_one("#app_browser")
+        if selected_option in self.lib.apps:
+            self.load_app(selected_option)
+            return
+        if selected_option=='← Back':
+            ol.clear_options()
+            if self.directory.endswith("/"):
+                self.directory=self.directory[:-1]
+            self.directory=os.path.dirname(self.directory)
+            if self.directory=="":
+                self.sub_title="Raspberry pi app store for open source projects"
+            else:
+                self.sub_title=self.directory
+            page=self.get_page_infos(self.directory)
+            ol.add_options([Option(e,id=e) for e in page])
+            self.appframe.revert()
+        elif self.directory_data[selected_option]["type"]=='Category':
+            ol.clear_options()
+            self.directory=self.directory_data[selected_option]["value"]
+            self.sub_title=self.directory
+            page=self.get_page_infos(self.directory_data[selected_option]["value"])
+            ol.add_options([Option(e,id=e) for e in page])
+            self.appframe.revert()
+        else:
+            self.load_app(selected_option)
+    @on(Input.Changed)
+    def search_for_app(self, event: Input.Changed):
+        self.search=event.value
+        ol = self.query_one("#app_browser")
+        if not self.search=="":
+            apps=self.lib.apps
+            keeped_apps=[app for app in apps if self.search.lower() in app.lower()]
+            ol.clear_options()
+            ol.add_options([Option(e,id=e) for e in keeped_apps])
+        else:
+            ol.clear_options()
+            if self.directory=="":
+                self.sub_title="Raspberry pi app store for open source projects"
+            else:
+                self.sub_title=self.directory
+            page=self.get_page_infos(self.directory)
+            ol.add_options([Option(e,id=e) for e in page])
+            self.appframe.revert()
+if __name__ == "__main__":
+    app = piapps()
+    app.run()
